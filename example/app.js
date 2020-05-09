5c3caf442219e53a2bbb3385d8a6b2eaff5de39d
3721 <yaoweijian@papegames.net>
2020-05-09 18:15:00 +0800

    update: nikki-player event

--- example/app.js
+++ example/app.js
@@ -2,5 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import MyPlayer from '../src'
 
-const App = () => <MyPlayer />
+const App = () =>
+  <MyPlayer 
+    src="https://nn4cdn.nikkigames.cn/xiaomaoqiu/b4cc53349ba8a218854457b377616172.mp4" 
+    targetUrl="https://nn4cdn.nikkigames.cn/nn4cn/mp4.html" 
+    style={{ maxHeight: '90vh', maxWidth: '90vw' }} />
 render(<App />, document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
 import React from 'react'
 
-const NikkiPlayer = () => (
-  <video src="https://nn4cdn.nikkigames.cn/nn4cn/video/pv-cn-wv-high.mp4"></video>
-)
+export default class NikkiPlayer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenu: false,
+      topState: 0,
+      leftState: 0,
+    }
+  }
 
-export default NikkiPlayer
+  componentWillReceiveProps(nextProps) {
+  }
+
+  componentDidMount() {
+  }
+
+  rightClick(e) {
+    const event = e || window.event
+    event.preventDefault()
+    // console.log('oncontextMenu:', event)
+    if (this.state.isMenu) {
+      this.setState({
+        isMenu: false
+      })
+    } else {
+      this.setState({
+        isMenu: true
+      })
+    }
+  }
+  openSrc2Url() {
+    const { src, targetUrl } = this.props
+    if (targetUrl) {
+      window.open(`${targetUrl}?video=${encodeURIComponent(src)}`)
+    } else {
+      window.open(src)
+    }
+  }
+
+  render() {
+    const { src, autoPlay, controls, width, height, poster, loop, preload, style } = this.props
+    const { isMenu, topState, leftState } = this.state
+    return (
+      <div className="nikki-player" style={style}>
+        <video
+          src={src}
+          autoPlay={autoPlay || true}
+          controls={controls || true}
+          poster={poster || '#'}
+          loop={loop || true}
+          preload={preload || 'true'}
+          width={width || '100%'}
+          height={height || '100%'}
+          onContextMenu={(e) => {this.rightClick(e); return false}}>
+        </video>
+        { isMenu ? 
+          (<div className="nikki-player-menu" style={{top: topState, left: leftState}}>
+            <ul>
+              <li onClick={() => {
+                this.openSrc2Url()
+              }}>查看视频外链</li>
+            </ul>
+          </div>) : '' }
+      </div>
+    )
+  }
+}
