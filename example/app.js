10bd1cac3aa811da588c84fbeed0b17aaf50da8a
3721 <yaoweijian@papegames.net>
2020-05-09 11:53:12 +0800

    feat: nikki component scaffolding

---
+++ example/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import MyPlayer from '../src'
+
+const App = () => <MyPlayer />
+render(<App />, document.getElementById('root'))
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NikkiPlayer = () => (
+  <video src="https://nn4cdn.nikkigames.cn/nn4cn/video/pv-cn-wv-high.mp4"></video>
+)
+
+export default NikkiPlayer
